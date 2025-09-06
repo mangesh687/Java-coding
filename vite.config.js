77ee478e4bbfa046bbf755775b8878caa6a5f401
mangesh687 <mangeshpakhale2016@gmail.com>
2025-09-06 19:38:48 +0530

    fix port and .env file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],server: {
-    port: 3000 // change this to your desired port
+  plugins: [react()],
+  server: {
+   port: parseInt(process.env.VITE_PORT) || 3000,
+    strictPort: true
   }
 })
